64b5844a08feab664ce1447f9e7ff9292b2768ba
DSAghicha <dsaghicha211@gmail.com>
2022-02-04 23:45:44 +0530

    fix: Revert Import to CommonJs Type & Configured CORS

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-import { config } from 'dotenv'
-config({path: './config.env'})
+require('dotenv').config({path: './config.env'})
 
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import connectDB from './config/db.js'
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/db')
 
 connectDB()
 
 const app = express()
-app.use(express.json())
-app.use(cookieParser())
-
 const PORT = process.env.PORT || 2022
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use('/auth', require('./routes/auth'))
 
 const server = app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
 
